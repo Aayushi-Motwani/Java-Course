def2b73eb924ae645ddeb817a4de1703538fc7d4
Aayushi Motwani <motwaniaayushi440@gmail.com>
2025-10-22 21:16:20 +0530

    A quick revision of Java basics to advance.

---
+++ SwapWithoutThirdVariable.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SwapWithoutThirdVariable {
+    public static void main(String[] args){
+
+        int a,b;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        b = sc.nextInt();
+
+        System.out.println("before swapping"); 
+        System.out.println("first number: " + a + " || Second Number: " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        // (OR this can be used)
+        // a = a * b;
+        // b = a / b;
+        // a = a / b;
+    
+        System.out.println("after swapping"); 
+        System.out.println("first number: " + a + " || Second Number: " + b);
+
+    }
+}
